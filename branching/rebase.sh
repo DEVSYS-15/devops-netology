2ed3a1bcb25a0335cc1e4a40923ba4f8695cc2b3
DEVSYS-15 <serzden@me.com>
2021-12-12 03:34:08 +0300

    done

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,14 +3,7 @@
 
 count=1
 for param in "$@"; do
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-   
->>>>>>> 79198f9 (git-rebase 1)
-=======
     echo "Next parameter: $param"
->>>>>>> 925c881 (git-rebase 2)
     count=$(( $count + 1 ))
 done
 
